2b7c4ac5ccbeba89b773bedaaa59cba8e85d3190
Saba Arefaine <arefainesaba@gmail.com>
2021-04-15 10:22:24 -0700

    temp converter

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("After Conversion:");
+            var f = TempConverter.CelsiusToFahrenheit(28);
+            var c = TempConverter.FahrenheitToCelsius(68);
+            
+           
+            Console.WriteLine($"28 degrees in Celsius is {f} degrees in Farenheit.");
+            Console.WriteLine($"68 degrees in Fahrenheit is {c} degrees in Celsius.");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+     //Create 2 methods inside the TempConverter class:
+     //Name the first method - FahrenheitToCelsius
+        public static double FahrenheitToCelsius (double f)
+        {
+            return (f - 32) / 1.8;
+        }
+
+     //Name the second method - CelsiusToFahrenheit
+        public static double CelsiusToFahrenheit (double c)
+        {
+            return (c * 1.8) + 32;
+        }
+
+
+        //Both methods will have a single parameter of type double
+        //Complete the logic so that they properly convert the temperatures
+        //Call both methods in your Main() method
+
+    }
+}
